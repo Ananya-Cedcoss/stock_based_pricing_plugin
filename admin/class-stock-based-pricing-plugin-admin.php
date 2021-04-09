06ef2146e5d8caa31add41de196aa5e30a11d6b1
ananya-cedcoss <ananyashukla@cedcoss.com>
2021-04-09 17:24:00 +0530

    Updated on 09-April-2021

--- admin/class-stock-based-pricing-plugin-admin.php
+++ admin/class-stock-based-pricing-plugin-admin.php
@@ -408,11 +408,8 @@ class Stock_based_pricing_plugin_Admin {
 	/**  The woocommerce_product_custom_fields frunction is used to create custom field */
 	public function woocommerce_product_custom_table_and_checkbox() {
 		global $post; // is used to get post object for the current post.
-<<<<<<< HEAD
-=======
 		
 		
->>>>>>> AnanyaShukla_addtocart_displayprice_SBPP
 		echo '<div class="options_group show_if_simple">'; // creation of div to hold checkbox.
 		woocommerce_wp_checkbox(
 			array(
@@ -425,10 +422,6 @@ class Stock_based_pricing_plugin_Admin {
 		echo '</div>'; // closing the div.
 
 		$sbpp_data       = get_post_meta( $post->ID, '_price_acc_to_stock' );// storing post meta of _price_acc_to_stock to sbpp_data variable.
-<<<<<<< HEAD
-		$sbpp_pricing    = json_decode( $sbpp_data[0], true );// it is used to encode it into array and store it to pricing.
-		$sbpp_count_data = count( $sbpp_pricing );// it is used to get number of data in array.
-=======
 
 		if ( ! empty( $sbpp_data ) ) {
 			$sbpp_pricing    = json_decode( $sbpp_data[0], true );// it is used to encode it into array and store it to pricing.
@@ -438,7 +431,6 @@ class Stock_based_pricing_plugin_Admin {
 			$sbpp_count_data = 0;
 		}
 
->>>>>>> AnanyaShukla_addtocart_displayprice_SBPP
 		echo '<div class=" product_custom_field show_if_simple "> '; // it is used to display the main div.
 		echo "<div id='my_stock_div' style='padding: 10px 160px;'>  <table id='Stock_table' ><tr> <th>Min Quanity </th>  <th>Max Quantity </th>  <th> Amount</th> </tr>"; // it is used to display the table header.
 
@@ -475,13 +467,6 @@ class Stock_based_pricing_plugin_Admin {
 	 * @param int $post_id is used to get the post id of current post.
 	 */
 	public function saving_dynamic_pricing( $post_id ) {
-<<<<<<< HEAD
-		$sbpp_min    = ( $_POST['Min'] ); // it is used to assign all Min value from the TextBox of Minimum Quantity.
-		$sbpp_max    = ( $_POST['Max'] ); // it is used to assign all Max value from the TextBox of Maximum Quantity.
-		$sbpp_amount = ( $_POST['Amount'] ); // it is used to assign all Amount value from the TextBox of Amount.
-		$sbpp_main_arry   = array(); // It is used to store sub array of Min,Max and Amount .
-		$sbpp_data_sub_array   = array(); // it is used to store Min, Max, Amount value.
-=======
 
 		$sbpp_min            = isset( $_POST['Min'] ) ? $_POST['Min'] : ''; // it is used to assign all Min value from the TextBox of Minimum Quantity.
 		$sbpp_max            = isset( $_POST['Max'] ) ? $_POST['Max'] : ''; // it is used to assign all Max value from the TextBox of Maximum Quantity.
@@ -490,17 +475,11 @@ class Stock_based_pricing_plugin_Admin {
 		$sbpp_data_sub_array = array(); // it is used to store Min, Max, Amount value.
 
 
->>>>>>> AnanyaShukla_addtocart_displayprice_SBPP
 
 		foreach ( $sbpp_min as $key => $value ) {
 			$sbpp_data_sub_array['Min']    = $value; // Assigning the Min value.
 			$sbpp_data_sub_array['Max']    = $sbpp_max[ $key ]; // Assigning the Max value.
 			$sbpp_data_sub_array['Amount'] = $sbpp_amount[ $key ]; // Assigning the Amount value.
-<<<<<<< HEAD
-
-			array_push( $sbpp_main_arry, $sbpp_data_sub_array ); // Push the sbpp_data_sub_array array to main array.
-		}
-=======
 			if ( ! empty($sbpp_data_sub_array['Min']) || ! empty( $sbpp_data_sub_array['Max'] ) || ! empty( $sbpp_data_sub_array['Amount'] ) ){
 				array_push( $sbpp_main_arry, $sbpp_data_sub_array ); // Push the sbpp_data_sub_array array to main array.
 		
@@ -510,16 +489,12 @@ class Stock_based_pricing_plugin_Admin {
 		if ( count( $sbpp_main_arry ) > 0 ) {
 
 
->>>>>>> AnanyaShukla_addtocart_displayprice_SBPP
 		$product = wc_get_product( $post_id ); // It is used to assign the data of product.
 		$product->update_meta_data( '_price_acc_to_stock', ( json_encode( $sbpp_main_arry ) ) ); // Updating the post meta data .
 		$woocommerce_custom_product_checkbox = isset( $_POST['_checkbox_for_stock_price'] ) ? 'yes' : 'no'; // assigning the value of checkbox.
 		update_post_meta( $post_id, '_checkbox_for_stock_price', $woocommerce_custom_product_checkbox ); // updating the value to post meta data.
 		$product->save(); // Saving the Product data.
-<<<<<<< HEAD
-=======
 		}
->>>>>>> AnanyaShukla_addtocart_displayprice_SBPP
 
 	}
 
@@ -540,13 +515,8 @@ class Stock_based_pricing_plugin_Admin {
 		$sbpp_pricing    = json_decode( $sbpp_data[0], true ); // decoding the data and converting it to array.
 		$sbpp_count_data = count( $sbpp_pricing ); // assigning the length of the array.
 
-<<<<<<< HEAD
-			echo '<div class=" product_custom_field show_if_variable"> '; // Displays the main div.
-			echo "<div id='my_stock_div_forVariation_" . esc_attr( $sbpp_index_loop ) . "' >  <table id='Stock_table_variation_" . esc_attr( $sbpp_index_loop ) . "' style='margin-left: -42px;' ><tr> <th>Min Quanity </th>  <th>Max Quantity </th>  <th> Amount</th> </tr>";// Display the Table Header.
-=======
 			echo '<div class=" product_custom_field show_if_variation_manage_stock"> '; // Displays the main div.
 			echo "<div id='my_stock_div_forVariation_" . esc_attr( $sbpp_index_loop ) . "' > <span> Give Price Acc To Stock </span> <br> <table id='Stock_table_variation_" . esc_attr( $sbpp_index_loop ) . "'  ><tr> <th>Min Quanity </th>  <th>Max Quantity </th>  <th> Amount</th> </tr>";// Display the Table Header.
->>>>>>> AnanyaShukla_addtocart_displayprice_SBPP
 
 		if ( $sbpp_count_data > 0 ) {
 			$sbpp_index = 1;
@@ -556,36 +526,20 @@ class Stock_based_pricing_plugin_Admin {
 				$sbpp_max_value   = $value['Max']; // Assigning the Max value.
 				$sbpp_amount      = $value['Amount']; // Assigning the Amount value.
 
-<<<<<<< HEAD
-				echo "<tr ><td> <input type='text' value='" . esc_attr( $sbpp_minimum_val ) . "'  name='Min_Var_" . esc_attr( $variation->ID ) . "[]' style='width:70%' id='Min_Quantity_Var_" . $sbpp_index_loop . $sbpp_index . "'   onkeypress='return AllowOnlyNumbers(event);'/>  </td>  "; // it displays the First td of table when data already exists.
-
-				echo " <td> <input type='text' value='" . esc_attr( $sbpp_max_value ) . "'  name='Max_Var_" . esc_attr( $variation->ID ) . "[]' onkeypress='return AllowOnlyNumbers(event);' onblur='validateMaxamount(this," . $sbpp_index_loop . $sbpp_index . ", -1, ".$loop.")' id='Max_Quantity_Var_" . $sbpp_index_loop . $sbpp_index . "' style='width:70%'/>  </td>"; // it displays the Second td of table when data already exists.
-
-				echo "<td> <input type='text' value='" . esc_attr( $sbpp_amount ) . "'  onkeypress='return AllowOnlyNumbers(event);'  name='Amount_Var_" . esc_attr( $variation->ID ) . "[]' id='Amount_Var_" . $sbpp_index_loop . $sbpp_index . "' style='width:70%'/>  </td></tr>"; // it displays the Third td of table when data already exists.
-=======
 				echo "<tr ><td> <input  type='text' value='" . esc_attr( $sbpp_minimum_val ) . "'  name='Min_Var_" . esc_attr( $variation->ID ) . "[]' style='min-width: fit-content;' id='Min_Quantity_Var_" . $sbpp_index_loop . $sbpp_index . "'   onkeypress='return AllowOnlyNumbers(event);'/>  </td>  "; // it displays the First td of table when data already exists.
 
 				echo " <td> <input type='text' value='" . esc_attr( $sbpp_max_value ) . "'  name='Max_Var_" . esc_attr( $variation->ID ) . "[]'  style='min-width: fit-content;'  onkeypress='return AllowOnlyNumbers(event);' onblur='validateMaxamount(this," . $sbpp_index_loop . $sbpp_index . ", -1, ".$loop.")' id='Max_Quantity_Var_" . $sbpp_index_loop . $sbpp_index . "' style='width:70%'/>  </td>"; // it displays the Second td of table when data already exists.
 
 				echo "<td> <input type='text' value='" . esc_attr( $sbpp_amount ) . "'  onkeypress='return AllowOnlyNumbers(event);'  name='Amount_Var_" . esc_attr( $variation->ID ) . "[]'  style='min-width: fit-content;'   id='Amount_Var_" . $sbpp_index_loop . $sbpp_index . "' style='width:70%'/>  </td></tr>"; // it displays the Third td of table when data already exists.
->>>>>>> AnanyaShukla_addtocart_displayprice_SBPP
 
 				$sbpp_index = ++$sbpp_index; // it is used to increase the index value by 1.
 			}
 		} else {
-<<<<<<< HEAD
-			echo "<tr ><td> <input type='text' name='Min_Var_" . esc_attr( $variation->ID ) . "[]' style='width:70%' onkeypress='return AllowOnlyNumbers(event);' id='Min_Quantity_Var_" . esc_attr( $sbpp_index_loop ) . "1'/>  </td> ";// it is used to display first td when there is no existing data.
-
-			echo " <td> <input type='text' onkeypress='return AllowOnlyNumbers(event);' name='Max_Var_" . esc_attr( $variation->ID ) . "[]' onblur='validateMaxamount(this," . $sbpp_index_loop . "1,-1,".$loop.")' onkeypress='return AllowOnlyNumbers(event);' id='Max_Quantity_Var_" . esc_attr( $sbpp_index_loop ) . "1' style='width:70%'/>  </td>";// it is used to display second td when there is no existing data.
-
-			echo " <td> <input type='text'   name='Amount_Var_" . esc_attr( $variation->ID ) . "[]' id='Amount_Var_" . esc_attr( $sbpp_index_loop ) . "1' style='width:70%'/>  </td></tr>";
-=======
 			echo "<tr ><td> <input type='text' name='Min_Var_" . esc_attr( $variation->ID ) . "[]' style='min-width: fit-content;' onkeypress='return AllowOnlyNumbers(event);' id='Min_Quantity_Var_" . esc_attr( $sbpp_index_loop ) . "1'/>  </td> ";// it is used to display first td when there is no existing data.
 
 			echo " <td> <input type='text' onkeypress='return AllowOnlyNumbers(event);' style='min-width: fit-content;' name='Max_Var_" . esc_attr( $variation->ID ) . "[]' onblur='validateMaxamount(this," . $sbpp_index_loop . "1,-1,".$loop.")' onkeypress='return AllowOnlyNumbers(event);' id='Max_Quantity_Var_" . esc_attr( $sbpp_index_loop ) . "1' style='width:70%'/>  </td>";// it is used to display second td when there is no existing data.
 
 			echo " <td> <input type='text'   name='Amount_Var_" . esc_attr( $variation->ID ) . "[]'   style='min-width: fit-content;'  id='Amount_Var_" . esc_attr( $sbpp_index_loop ) . "1' style='width:70%'/>  </td></tr>";
->>>>>>> AnanyaShukla_addtocart_displayprice_SBPP
 		}
 		echo "</table><span style='padding: 10px;color:#2271b1' onclick='GenerateNewRow_Variation(" . esc_attr( $loop ) . ", " . esc_attr( $variation->ID ) . ")'><u>Add New Tier </u></span></div></div>";// it is used to display third td when there is no existing data.
 	}
@@ -598,19 +552,11 @@ class Stock_based_pricing_plugin_Admin {
 	 * @param                                    int $variation_id is the Id of current variation.
 	 * @param                                    int $i is the index of the current variation.
 	 */
-<<<<<<< HEAD
-	public function sbp_save_custom_field_variations( $variation_id, $i ) {
-
-		$sbpp_min_alldatavariation    = ( $_POST['Min_Var_' . $variation_id] ); // assign all minimum value to the min variable.
-		$sbpp_max_alldatavariation    = ( $_POST['Max_Var_' . $variation_id] ); // assign all maximum value to the max variable.
-		$sbpp_amount_alldatavariation = ( $_POST['Amount_Var_' . $variation_id] ); // assign all amount value to the amount variable.
-=======
 	public function sbp_save_custom_field_variations( $variation_id, $i ) {	
 		
 		$sbpp_min_alldatavariation    = isset( $_POST['Min_Var_' . $variation_id] ) ? $_POST['Min_Var_' . $variation_id] : ''; // assign all minimum value to the min variable.
 		$sbpp_max_alldatavariation    = isset( $_POST['Max_Var_' . $variation_id] ) ? $_POST['Max_Var_' . $variation_id] : ''; // assign all maximum value to the max variable.
 		$sbpp_amount_alldatavariation = isset( $_POST['Amount_Var_' . $variation_id] ) ? $_POST['Amount_Var_' . $variation_id] : ''; // assign all amount value to the amount variable.
->>>>>>> AnanyaShukla_addtocart_displayprice_SBPP
 		$sbpp_main_array_variation    = array(); // It is used to store sub array of Min,Max and Amount .
 		$sbpp_data_subarray_variation = array(); // it is used to store Min, Max, Amount value.
 		foreach ( $sbpp_min_alldatavariation as $key => $value ) {
@@ -624,8 +570,4 @@ class Stock_based_pricing_plugin_Admin {
 
 		die(); // this is required to terminate immediately and return a proper response.
 	}
-<<<<<<< HEAD
-=======
-
->>>>>>> AnanyaShukla_addtocart_displayprice_SBPP
 }
